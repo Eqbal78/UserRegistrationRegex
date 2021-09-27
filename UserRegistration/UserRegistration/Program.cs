73e9b3df29eddb4a94d4e5a57eddde268b880fbc
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-27 18:18:06 +0530

    Adding First Name in Regex Method

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------Welcome to User Registration Problem--------------------");
+            //object create
+            Registrations user = new Registrations();
+            Console.WriteLine("Enter your first name : ");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/Registrations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class Registrations
+    {
+        /// <summary>
+        /// Check the string is following pattern or not
+        /// </summary>
+        /// <param name="name"></param>
+        public void Check(string name)
+        {
+            // Regex pattern
+            string pattern = "^[A-Z]{1}[a-z]{4}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine("Name {0} is Valid ", name);
+            }
+            else
+            {
+                Console.WriteLine("Name {0} is Invalid", name);
+            }
+        }
+    }
+}
